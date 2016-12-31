bbf42f40a22958456947cad238b2324b52f2a4d0
Alin Popa <alin.popa@gmail.com>
2016-12-31 12:29:40 +0000

    Add the last pressure value.

--- data/data.go
+++ data/data.go
@@ -23,11 +23,12 @@ type IpInfo struct {
 }
 
 type WeatherInfo struct {
-	Temp     float32 `json:"temp"`
-	Pressure float32 `json:"pressure"`
-	Day      bool    `json:"day"`
-	Humidity float32 `json:"humid"`
-	Lux      float32 `json:"lux"`
+	Temp         float32 `json:"temp"`
+	Pressure     float32 `json:"pressure"`
+	Day          bool    `json:"day"`
+	Humidity     float32 `json:"humid"`
+	Lux          float32 `json:"lux"`
+	LastPressure float32 `json:"lastPressure"`
 }
 
 type RtmResponse struct {
